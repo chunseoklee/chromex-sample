a2d5e70971971419e7663782633e43d8fffa29c2
twoflower <chunseoklee@gmail.com>
2019-06-21 13:43:05 +0900

    Init Code

--- src/background/chromex_sample/background/core.cljs
+++ src/background/chromex_sample/background/core.cljs
@@ -2,76 +2,24 @@
   (:require-macros [cljs.core.async.macros :refer [go go-loop]])
   (:require [goog.string :as gstring]
             [goog.string.format]
-            [cljs.core.async :refer [<! chan]]
+            [cljs.core.async :refer [<! chan timeout]]
             [chromex.logging :refer-macros [log info warn error group group-end]]
             [chromex.chrome-event-channel :refer [make-chrome-event-channel]]
             [chromex.protocols.chrome-port :refer [post-message! get-sender]]
             [chromex.ext.tabs :as tabs]
             [chromex.ext.runtime :as runtime]
+            [chromex.ext.windows :as windows]
             [chromex-sample.background.storage :refer [test-storage!]]))
 
 (def clients (atom []))
 
-; -- clients manipulation ---------------------------------------------------------------------------------------------------
 
-(defn add-client! [client]
-  (log "BACKGROUND: client connected" (get-sender client))
-  (swap! clients conj client))
-
-(defn remove-client! [client]
-  (log "BACKGROUND: client disconnected" (get-sender client))
-  (let [remove-item (fn [coll item] (remove #(identical? item %) coll))]
-    (swap! clients remove-item client)))
-
-; -- client event loop ------------------------------------------------------------------------------------------------------
-
-(defn run-client-message-loop! [client]
-  (log "BACKGROUND: starting event loop for client:" (get-sender client))
+(defn run-notification-loop []
   (go-loop []
-    (when-some [message (<! client)]
-      (log "BACKGROUND: got client message:" message "from" (get-sender client))
-      (recur))
-    (log "BACKGROUND: leaving event loop for client:" (get-sender client))
-    (remove-client! client)))
-
-; -- event handlers ---------------------------------------------------------------------------------------------------------
-
-(defn handle-client-connection! [client]
-  (add-client! client)
-  (post-message! client "hello from BACKGROUND PAGE!")
-  (run-client-message-loop! client))
-
-(defn tell-clients-about-new-tab! []
-  (doseq [client @clients]
-    (post-message! client "a new tab was created")))
-
-; -- main event loop --------------------------------------------------------------------------------------------------------
-
-(defn process-chrome-event [event-num event]
-  (log (gstring/format "BACKGROUND: got chrome event (%05d)" event-num) event)
-  (let [[event-id event-args] event]
-    (case event-id
-      ::runtime/on-connect (apply handle-client-connection! event-args)
-      ::tabs/on-created (tell-clients-about-new-tab!)
-      nil)))
-
-(defn run-chrome-event-loop! [chrome-event-channel]
-  (log "BACKGROUND: starting main event loop...")
-  (go-loop [event-num 1]
-    (when-some [event (<! chrome-event-channel)]
-      (process-chrome-event event-num event)
-      (recur (inc event-num)))
-    (log "BACKGROUND: leaving main event loop")))
-
-(defn boot-chrome-event-loop! []
-  (let [chrome-event-channel (make-chrome-event-channel (chan))]
-    (tabs/tap-all-events chrome-event-channel)
-    (runtime/tap-all-events chrome-event-channel)
-    (run-chrome-event-loop! chrome-event-channel)))
-
-; -- main entry point -------------------------------------------------------------------------------------------------------
+    (<! (timeout 10000))
+    (windows/create (js-obj "url" "https://github.sec.samsung.net/notifications"))
+    (recur)))
 
 (defn init! []
   (log "BACKGROUND: init")
-  (test-storage!)
-  (boot-chrome-event-loop!))
+  (run-notification-loop))
