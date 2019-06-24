1f18b67a1709309873bbf34f1521cec781c28778
twoflower <chunseoklee@gmail.com>
2019-06-24 10:18:19 +0900

    Notification

--- src/background/chromex_sample/background/core.cljs
+++ src/background/chromex_sample/background/core.cljs
@@ -5,20 +5,23 @@
             [cljs.core.async :refer [<! chan timeout]]
             [chromex.logging :refer-macros [log info warn error group group-end]]
             [chromex.chrome-event-channel :refer [make-chrome-event-channel]]
-            [chromex.protocols.chrome-port :refer [post-message! get-sender]]
             [chromex.ext.tabs :as tabs]
             [chromex.ext.runtime :as runtime]
             [chromex.ext.windows :as windows]
-            [chromex-sample.background.storage :refer [test-storage!]]))
+            [chromex.ext.notifications :as noti]))
 
 (def clients (atom []))
 
-
 (defn run-notification-loop []
-  (go-loop []
+  (go-loop [noto-id 1]
     (<! (timeout 10000))
-    (windows/create (js-obj "url" "https://github.sec.samsung.net/notifications"))
-    (recur)))
+    ;;(windows/create (js-obj "url" "https://github.sec.samsung.net/notifications"))
+    (noti/create (str noti-id) (js-obj
+                     "type" "basic"
+                     "iconUrl" "../images/notification.png"
+                     "message" "Hey, Get your review done !"
+                     "title" "review time"))
+    (recur (inc noti-id))))
 
 (defn init! []
   (log "BACKGROUND: init")
